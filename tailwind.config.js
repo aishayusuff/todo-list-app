63083b8507a57ec2b4d1ec813a58abb623021b45
aishayusuff <aishatyusuffe@gmail.com>
2024-02-25 22:15:47 +0100

    additional styling config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'primary-color': '#000',
+          'secondary-color': '#e8caca',
+          'tert-color': '#280909',
+        }
+      },
     },
     plugins: [],
   }
