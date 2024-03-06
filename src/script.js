7077f35c1f9a27009f3e82661c5f2cc1d02201f4
aishayusuff <aishatyusuffe@gmail.com>
2024-03-06 22:05:57 +0100

    refactors callback and adds edge case

--- src/script.js
+++ src/script.js
@@ -3,19 +3,29 @@ const btn = document.querySelector(".btn");
 const todoList = document.querySelector(".todo");
 
 
-
-
-btn.addEventListener("click", () => {
-    const html = ` 
+//adds new task to todoList
+const addsTask = function() {
+    let html = ''
+    
+    //edge case
+    if(inputBox.value === '') {
+        alert("Input cannot be left blank!")
+        return;
+    } else {
+        html = ` 
         <li class="select-none px-3 pt-3 pb-4 ">
             <label class="">
                 <input type="checkbox" class="form-checkbox h-3 w-3 cursor-pointer checked:line-through">
                 <span class="select-none">${inputBox.value}</span>
             </label>
         </li>`
-    
+    }
+
     todoList.insertAdjacentHTML("afterbegin", html);
-})
+}
+
+
+btn.addEventListener("click", addsTask )
 
 
 
