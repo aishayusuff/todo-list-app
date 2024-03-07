60e8b74d9a8796f15dd27715c57c7c7c20e9c52c
aishayusuff <aishatyusuffe@gmail.com>
2024-03-07 13:00:18 +0100

    modifies structure of dynamically created html

--- src/script.js
+++ src/script.js
@@ -16,12 +16,22 @@ const addsTask = function() {
         return;
     } else {
         html = ` 
-        <li class="select-none px-3 pt-3 pb-4 ">
-            <label class="">
-                <input type="checkbox" class="form-checkbox h-3 w-3 cursor-pointer checked:line-through">
-                <span class="select-none">${inputBox.value}</span>
-            </label>
-        </li>`
+        <label>
+            <input type="checkbox" class="form-checkbox h-3 w-3 cursor-pointer checked:line-through">
+            <span class="select-none">${inputBox.value}</span>
+        </label>`
+
+        //Creates li element and add class styling
+        let li = document.createElement("li");
+        li.classList.add("select-none px-3 pt-3 pb-4");
+
+        //Set innerHTML of new li element to html
+        li.innerHTML = html;
+
+        //Creates new span for delete btn and adds after text span
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.querySelector("span").appendChild(span);
     }
 
     todoList.insertAdjacentHTML("afterbegin", html);
