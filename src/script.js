49170d731b36aa796cd1731d8e951227c142c22a
aishayusuff <aishatyusuffe@gmail.com>
2024-03-07 13:18:26 +0100

    fixes some bugs with the dynamic html

--- src/script.js
+++ src/script.js
@@ -8,14 +8,13 @@ todoList.innerHTML = '';
 
 //adds new task to todoList
 const addsTask = function() {
-    let html = ''
 
     //edge case
     if(inputBox.value === '') {
         alert("Input cannot be left blank!")
         return;
     } else {
-        html = ` 
+        const html = ` 
         <label>
             <input type="checkbox" class="form-checkbox h-3 w-3 cursor-pointer checked:line-through">
             <span class="select-none">${inputBox.value}</span>
@@ -23,7 +22,7 @@ const addsTask = function() {
 
         //Creates li element and add class styling
         let li = document.createElement("li");
-        li.classList.add("select-none px-3 pt-3 pb-4");
+        li.classList.add("select-none", "px-3", "pt-3", "pb-4");
 
         //Set innerHTML of new li element to html
         li.innerHTML = html;
@@ -31,11 +30,11 @@ const addsTask = function() {
         //Creates new span for delete btn and adds after text span
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
-        li.querySelector("span").appendChild(span);
-    }
+        li.querySelector("label").appendChild(span);
 
-    todoList.insertAdjacentHTML("afterbegin", html);
-    inputBox.value = '';
+        todoList.insertAdjacentElement("afterbegin", li);
+        inputBox.value = '';
+    }
 }
 
 
