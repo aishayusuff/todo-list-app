a7b14ab7c6071098e08531f7fc55ebb45bfc059c
aishayusuff <aishatyusuffe@gmail.com>
2024-03-10 15:52:40 +0100

    removes task from local storage on clicking close btn

--- src/script.js
+++ src/script.js
@@ -3,77 +3,104 @@ const btn = document.querySelector(".btn");
 const todoList = document.querySelector(".todo");
 
 
-const getTasksFromLocalStorage = function() {
-    const taskJSON = localStorage.getItem("tasks");
-    const task = JSON.parse(taskJSON)
-    return task || []
-}
+const getTasksFromLocalStorage = function () {
+  const taskJSON = localStorage.getItem("tasks");
+  const task = JSON.parse(taskJSON);
+  return task || [];
+};
 
-const setTasksToLocalStorage = function(tasks){
-    const taskJSON = JSON.stringify(tasks);
-    localStorage.setItem("tasks", taskJSON)
-}
+const setTasksToLocalStorage = function (tasks) {
+  const taskJSON = JSON.stringify(tasks);
+  localStorage.setItem("tasks", taskJSON);
+};
 
-const displayTasksFromLocalStorage = function() {
-    const tasks = getTasksFromLocalStorage();
- 
-    if (tasks.length > 0) {
-        tasks.forEach(task => {
-            createsHTML(task)
-        })
-    }
-}
+const removesTaskFromLocalStorage = function (taskIndex) {
+  const tasks = getTasksFromLocalStorage();
 
-const createsHTML = function(userInput) {
-    const html = ` 
+  tasks.splice(taskIndex, 1);
+  setTasksToLocalStorage(tasks)
+};
+
+const displayTasksFromLocalStorage = function () {
+  const tasks = getTasksFromLocalStorage();
+
+  if (tasks.length > 0) {
+    tasks.forEach((task, index) => {
+      createsHTML(task, index);
+    });
+  }
+};
+
+const createsHTML = function (userInput, index) {
+  const html = ` 
     <label>
         <input type="checkbox" class="form-checkbox h-3 w-3 mr-5 cursor-pointer checked:line-through">
         <span class="select-none cursor-pointer">${userInput}</span>
-    </label>`
+    </label>`;
 
-    //Creates li element and add class styling
-    let li = document.createElement("li");
-    li.classList.add("select-none", "px-3", "pt-3", "flex", "justify-between", "items-center");
+  //Creates li element and add class styling
+  let li = document.createElement("li");
+  li.classList.add(
+    "select-none",
+    "px-3",
+    "pt-3",
+    "flex",
+    "justify-between",
+    "items-center"
+  );
 
-    //Set innerHTML of new li element to html
-    li.innerHTML = html;
+  //sets task index as an attribute 
+  li.setAttribute("data-task-index", index)
 
-    //Creates new span for delete btn and adds after text span
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    span.classList.add("close-btn","text-tert-color", "text-center")
-     li.appendChild(span);
+  //Set innerHTML of new li element to html
+  li.innerHTML = html;
 
-    return todoList.insertAdjacentElement("afterbegin", li);
-}
+  //Creates new span for delete btn and adds after text span
+  let span = document.createElement("span");
+  span.innerHTML = "\u00d7";
+  span.classList.add("close-btn", "text-tert-color", "text-center");
+  li.appendChild(span);
+
+  return todoList.insertAdjacentElement("afterbegin", li);
+};
 
 //adds new task to todoList
-const addsTask = function() {
-    let userInput = inputBox.value.trim();
+const addsTask = function () {
+  let userInput = inputBox.value.trim();
 
-    if(userInput === '') {
-        alert("Input cannot be left blank!")
-        return;
-    } else {
+  if (userInput === "") {
+    alert("Input cannot be left blank!");
+    return;
+  } else {
+    //stores input in local storage
+    let tasks = getTasksFromLocalStorage();
+    tasks.push(userInput);
 
-        //stores input in local storage
-        let tasks = getTasksFromLocalStorage();
-        tasks.push(userInput);
-        console.log(tasks)
-        setTasksToLocalStorage(tasks);
+    setTasksToLocalStorage(tasks);
 
-       createsHTML(userInput);
-       inputBox.value = '';
-    }
-}
+    createsHTML(userInput, tasks.length - 1);
+    inputBox.value = "";
+  }
+};
 
+displayTasksFromLocalStorage();
 //Adds new task on btn click
-btn.addEventListener("click", addsTask)
+btn.addEventListener("click", addsTask);
+
+document.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    addsTask();
+  }
+});
 
 //Deletes item when close btn clicked
 todoList.addEventListener("click", (e) => {
-    if (!e.target.classList.contains("close-btn")) {
-       return
-    }    
-    e.target.parentElement.remove();
-})
+  if (!e.target.classList.contains("close-btn")) {
+    return;
+  }
+  
+  const taskIndex = e.target.parentElement.getAttribute("data-task-index")
+  removesTaskFromLocalStorage(taskIndex);
+
+  e.target.parentElement.remove();
+});
